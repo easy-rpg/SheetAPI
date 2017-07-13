2219d13b8059c8127752c9c3b2a9a8ada9eea2c3
RodrigoNdeC <rodrigondec@gmail.com>
2017-07-13 03:31:57 -0300

    adicionado g e session

--- rpg/__init__.py
+++ rpg/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, g, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
@@ -27,6 +27,8 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
+from rpg.core.models import *
+
 @app.errorhandler(404)
 def not_found(error):
     app.logger.error(error)
@@ -37,7 +39,12 @@ def not_authorized(error):
     app.logger.error(error)
     return error
 
-from rpg.core import models
+@app.before_request
+def load_current_user():
+    if 'id_usuario' in session:
+        g.usuario = Usuario.query.filter_by(id_usuario=session['id_usuario']).first()
+    else:
+        g.usuario = None
 
 from rpg.mestre.controllers import mod_mestre as mestre_module
 from rpg.jogador.controllers import mod_jogador as jogador_module
