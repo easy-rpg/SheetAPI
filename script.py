b28d187ed0216a4af9535c61244e467700971487
RodrigoNdeC <rodrigondec@gmail.com>
2017-07-13 16:50:19 -0300

    minor fix

--- script.py
+++ script.py
@@ -1,8 +1,9 @@
 from rpg.core.models import *
 from rpg import db
 
-rodrigo = Usuario(nome='Rodrigo', email='rodrigondec@gmail.com', senha='rodrigo123')
-db.session.add(rodrigo)
+# rodrigo = Usuario(nome='Rodrigo', email='rodrigondec@gmail.com', senha='rodrigo123')
+rodrigo = Usuario.query.filber_by(email='rodrigondec@gmail.com').first()
+# db.session.add(rodrigo)
 
 
 
