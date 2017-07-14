e08360ea5b397813778ef2f9d228ba4b1869e172
RodrigoNdeC <rodrigondec@gmail.com>
2017-07-14 15:44:08 -0300

    minor fix

--- rpg/__init__.py
+++ rpg/__init__.py
@@ -27,17 +27,21 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
+
 from rpg.core.models import *
+
 
 @app.errorhandler(404)
 def not_found(error):
     app.logger.error(error)
     return error
 
+
 @app.errorhandler(403)
 def not_authorized(error):
     app.logger.error(error)
     return error
+
 
 @app.before_request
 def load_current_user():
@@ -45,6 +49,7 @@ def load_current_user():
         g.usuario = Usuario.query.filter_by(id_usuario=session['id_usuario']).first()
     else:
         g.usuario = None
+
 
 from rpg.core.controllers import mod_core as core_module
 from rpg.mestre.controllers import mod_mestre as mestre_module
