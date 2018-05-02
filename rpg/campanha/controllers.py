7c0d3b94e1e930540d5cd49432271be88bde1801
RodrigoNdeC <rodrigondec@gmail.com>
2018-05-02 18:09:59 -0300

    remove flask

--- rpg/campanha/controllers.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-mod_campanha = Blueprint('campanha', __name__, url_prefix='/campanha')
-
-@mod_campanha.route('/')
-def index():
-    return render_template('campanha/index.html')
--- run.py
+++
@@ -1,4 +0,0 @@
-from rpg import app
-
-app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
-app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
