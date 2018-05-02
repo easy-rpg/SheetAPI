fbfe17ebb3e4d3f156596b458994942fd66693e8
RodrigoNdeC <rodrigondec@gmail.com>
2018-05-02 18:10:50 -0300

    django iniciado

---
+++ SheetAPI/config_example.py
@@ -0,0 +1,17 @@
+# DB Heroku
+# DB_HOST = "ec2-54-235-123-159.compute-1.amazonaws.com"
+# DB_PORT = "5432"
+#
+# DB_NAME = "dafelg3fd81h3m"
+#
+# DB_USER = "zamkezdjhbexco"
+# DB_PASSWORD = "15083a2d7e83bce97f46cffb22cc0aece4d7c0cf70e0800eeeda804e07d13881"
+
+# DB LOCAL
+DB_HOST = "localhost"
+DB_PORT = ""
+
+DB_NAME = "DB_NAME"
+
+DB_USER = "DB_USER"
+DB_PASSWORD = ""
