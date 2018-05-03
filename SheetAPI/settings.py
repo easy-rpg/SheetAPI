5cb84f3198547b5616ec0a5c60a76fc78a33df3a
RodrigoNdeC <rodrigondec@gmail.com>
2018-05-02 22:30:57 -0300

    import settings

--- SheetAPI/settings.py
+++ SheetAPI/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from .config import DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
