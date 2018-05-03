1310372306adce727e90ae0f0388364344307f83
RodrigoNdeC <rodrigondec@gmail.com>
2018-05-02 22:30:49 -0300

    Models ok

--- core/models.py
+++ core/models.py
@@ -5,7 +5,11 @@ from model_utils import Choices
 
 # Create your models here.
 class Tendencia(Model):
-    valor = CharField()
+    valor = CharField(max_length=16, unique=True)
+    slug = CharField(max_length=3, unique=True)
+
+    def __str__(self):
+        return self.valor
 
 class BBA(Model):
     nivel = IntegerField(
@@ -23,11 +27,19 @@ class BBA(Model):
     QUALIDADE = Choices(('boa', ('BBA Boa')), ('ruim', ('BBA Ruim')))
     qualidade = CharField(choices=QUALIDADE, max_length=4)
 
+    def __str__(self):
+        return 'BBA {} nível {}'.format(self.qualidade, self.nivel)
 
 class Atributo(Model):
-    nome = CharField()
-    slug = CharField()
+    NOME = Choices(('Força'), ('Destreza'), ('Constituição'),
+                      ('Inteligência'), ('Sabedoria'), ('Carisma'))
+    nome = CharField(choices=NOME, max_length=12, unique=True)
+    SLUG = Choices(('for'), ('des'), ('con'),
+                      ('int'), ('sab'), ('car'))
+    slug = CharField(choices=SLUG, max_length=3, unique=True)
 
+    def __str__(self):
+        return self.nome
 
 class Resistencia(Model):
     nivel = IntegerField(
@@ -42,24 +54,31 @@ class Resistencia(Model):
             MinValueValidator(0)
         ]
     )
-    TIPO = Choices(('fort', ('Fortitude')), ('ref', ('Reflexo')), ('von', ('Vontade')))
-    tipo = CharField(choices=TIPO, max_length=4)
+    NOME =Choices(('Fortitude'), ('Reflexo'), ('Vontade'))
+    nome = CharField(choices=NOME, max_length=9)
+    SLUG = Choices(('fort'), ('ref'), ('von'))
+    slug = CharField(choices=SLUG, max_length=4)
     QUALIDADE = Choices(('boa', ('Resistencia Boa')), ('ruim', ('Resistencia Ruim')))
     qualidade = CharField(choices=QUALIDADE, max_length=4)
-    atributo = OneToOneField(Atributo, related_name='+')
+    atributo = ForeignKey(Atributo, related_name='+', on_delete=PROTECT)
 
+    class Meta:
+        unique_together = ('slug', 'qualidade', 'nivel', 'valor')
+
+    def __str__(self):
+        return '{} {} nivel {}'.format(self.nome, self.qualidade, self.nivel)
 
 class Pericia(Model):
-    nome = CharField()
-    slug = CharField()
-    # ATRIBUTO = Choices(('for', ('Força')), ('des', ('Destreza')), ('con', ('Constituição')),
-    #                   ('int', ('Inteligência')), ('sab', ('Sabedoria')), ('car', ('Carisma')))
-    # atributo = CharField(choices=ATRIBUTO, max_length=3)
+    nome = CharField(max_length=24)
+    slug = CharField(max_length=24)
     atributo = ForeignKey(Atributo, related_name='+', on_delete=PROTECT)
+
+    def __str__(self):
+        return self.nome
 
 
 class Classe(PolymorphicModel):
-    nome = CharField()
+    nome = CharField(max_length=20)
     pericias_disponiveis = ManyToManyField(Pericia, related_name='+')
     quantidade_pericias_por_nivel = IntegerField(
         validators=[
@@ -69,7 +88,7 @@ class Classe(PolymorphicModel):
     BBAs = ManyToManyField(BBA, related_name='+')
     tendencias = ManyToManyField(Tendencia, related_name='+')
     DV = Choices((4, ('d4')), (6, ('d6')),(8, ('d8')), (10, ('d10')), (12, ('d12')))
-    dv = IntegerField(choices=DV, max_length=2)
+    dv = IntegerField(choices=DV)
     CONJURADOR = Choices(('div', ('Divino')), ('arc', ('Arcano')), ('nan', ('Não conjurador')))
     conjurador = CharField(choices=CONJURADOR, default=CONJURADOR.nan, max_length=3)
     # conjurador_completo = BooleanField(default=True)
