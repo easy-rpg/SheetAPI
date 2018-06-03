9ede491fe7441f4aae14344ad1233a8955f914b7
RodrigoNdeC <rodrigondec@gmail.com>
2018-06-03 03:44:35 -0300

    minor fix

--- campanha/models.py
+++ campanha/models.py
@@ -6,7 +6,13 @@ class Campanha(Model):
     nome = CharField(max_length=30, unique=True)
     mestre = ForeignKey(PerfilUsuario, on_delete=SET_NULL, null=True, related_name='campanhas')
 
+    def __str__(self):
+        return self.nome
+
 
 class Arco(Model):
     nome = CharField(max_length=30, unique=True)
     campanha = ForeignKey(Campanha, on_delete=CASCADE, related_name='arcos')
+
+    def __str__(self):
+        return '{} | {}'.format(self.campanha, self.nome)
