0a072799bf0b42159564e383205dbd0641c526c1
RodrigoNdeC <rodrigondec@gmail.com>
2018-06-03 03:45:57 -0300

    minor fix

--- personagem/admin.py
+++ personagem/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Personagem, Ficha
+from .models import Personagem, PersonagemClasse
 
 
 admin.site.register(Personagem)
-admin.site.register(Ficha)
+admin.site.register(PersonagemClasse)
