be461cae63b321a7e55f2e9f6ab21154e534e91b
RodrigoNdeC <rodrigondec@gmail.com>
2018-06-04 03:24:50 -0300

    add admin

--- personagem/admin.py
+++ personagem/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Personagem, PersonagemClasse
+from .models import Personagem, PersonagemClasse, PersonagemModelo, PersonagemAtributo
 
 
 admin.site.register(Personagem)
 admin.site.register(PersonagemClasse)
+admin.site.register(PersonagemModelo)
+admin.site.register(PersonagemAtributo)
