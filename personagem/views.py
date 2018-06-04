78ae3520fab59d328858f0b76bbcb785d6b1f453
RodrigoNdeC <rodrigondec@gmail.com>
2018-06-04 03:24:24 -0300

    views de personagem

--- personagem/views.py
+++ personagem/views.py
@@ -1,22 +1,23 @@
 from rest_framework.viewsets import ModelViewSet
-from .models import Personagem, PersonagemClasse
-from .serializers import PersonagemSerializer, PersonagemClasseSerializer
+from .models import Personagem, PersonagemClasse, PersonagemModelo, PersonagemAtributo
+from .serializers import PersonagemSerializer, PersonagemClasseSerializer, PersonagemModeloSerializer, \
+    PersonagemAtributoSerializer
 
 
 class PersonagemViewSet(ModelViewSet):
     """
     retrieve:
-        Retorna um usuário
+        Retorna um personagem
     list:
-        Retorna todos os usuários
+        Retorna todos os personagens
     create:
-        Cria um novo usuário
+        Cria um novo personagem
     delete:
-        Remove um usuário existente
+        Remove um personagem existente
     partial_update:
-        Atualiza um ou mais campos de um usuário existente
+        Atualiza um ou mais campos de um personagem existente
     update:
-        Atualiza um usuário
+        Atualiza um personagem
     """
     # permission_classes = [
     #     IsAuthenticated,
@@ -24,3 +25,37 @@ class PersonagemViewSet(ModelViewSet):
     queryset = Personagem.objects.all()
     serializer_class = PersonagemSerializer
     http_method_names = ['get', 'post', 'put', 'patch', 'delete']
+
+
+class PersonagemClasseViewSet(ModelViewSet):
+    """
+    create:
+        Adiciona uma nova classe a um personagem existente
+    partial_update:
+        Atualiza um ou mais campos de uma classe de um personagem existente
+    """
+    queryset = PersonagemClasse.objects.all()
+    serializer_class = PersonagemClasseSerializer
+    http_method_names = ['post', 'patch']
+
+
+class PersonagemModeloViewSet(ModelViewSet):
+    """
+    create:
+        Adiciona um novo modelo a um personagem existente
+    partial_update:
+        Atualiza um ou mais campos de um modelo de um personagem existente
+    """
+    queryset = PersonagemModelo.objects.all()
+    serializer_class = PersonagemModeloSerializer
+    http_method_names = ['post', 'patch']
+
+
+class PersonagemAtributoViewSet(ModelViewSet):
+    """
+    partial_update:
+        Atualiza um ou mais campos de um atributo de um personagem existente
+    """
+    queryset = PersonagemAtributo.objects.all()
+    serializer_class = PersonagemAtributoSerializer
+    http_method_names = ['patch']
