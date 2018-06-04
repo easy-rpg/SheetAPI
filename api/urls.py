32f95919246336c2a43a0f893906ba29eb0d6e7f
RodrigoNdeC <rodrigondec@gmail.com>
2018-06-03 22:32:58 -0300

    urls ok

--- api/urls.py
+++ api/urls.py
@@ -3,12 +3,16 @@ from rest_framework import routers
 from rest_framework_swagger.views import get_swagger_view
 from .views import UserViewSet
 from campanha.views import CampanhaViewSet, ArcoViewSet
+from core.views import ClasseViewset
+from personagem.views import PersonagemViewSet
 
 
 router = routers.DefaultRouter()
 router.register('users', UserViewSet)
 router.register('campanhas', CampanhaViewSet)
 router.register('arcos', ArcoViewSet)
+router.register('classes', ClasseViewset)
+router.register('personagems', PersonagemViewSet)
 
 schema_view = get_swagger_view(title='LocationTracker API')
 
