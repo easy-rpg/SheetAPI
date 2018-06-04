48cd1b31c735a4f94b65f13042c66ce709e4b960
RodrigoNdeC <rodrigondec@gmail.com>
2018-06-04 03:23:52 -0300

    PersonagemAtributo e PersonagemModelo

--- personagem/models.py
+++ personagem/models.py
@@ -1,10 +1,11 @@
 from django.db.models import Model, ForeignKey, ManyToManyField, \
     CharField, IntegerField, FloatField, TextField, ImageField, \
     CASCADE, SET_NULL, PROTECT
+from django.db.models.signals import post_save
 from model_utils import Choices
 from django.contrib.auth.models import User
 from campanha.models import Arco
-from core.models import Tendencia, Raca, Classe, Modelo
+from core.models import Atributo, Tendencia, Raca, Classe, Modelo
 from .utils import get_personagem_upload_path
 
 
@@ -29,35 +30,45 @@ class Personagem(Model):
 
     lore = TextField(null=True, blank=True)
 
+    lista_atributos = ManyToManyField(
+        Atributo,
+        through='PersonagemAtributo',
+        related_name='+'
+    )
     tendencia = ForeignKey(Tendencia, on_delete=PROTECT, related_name='+')
     raca = ForeignKey(Raca, on_delete=PROTECT, related_name='+')
-    classes = ManyToManyField(
+    lista_classes = ManyToManyField(
         Classe,
         through='PersonagemClasse',
         related_name='+'
     )
-    modelos = ManyToManyField(Modelo, blank=True)
+    lista_modelos = ManyToManyField(
+        Modelo,
+        through='PersonagemModelo',
+        related_name='+',
+        blank=True
+    )
 
     def __str__(self):
         return self.nome
 
     @property
     def bba(self):
-        print('getting bba')
+        # print('getting bba')
         bba_atual = 0
-        print('bba_atual: {}'.format(bba_atual))
-        for personagem_classe in self.personagem_classes.all():
-            print('personagem_classe: {}'.format(personagem_classe))
-            print('bba do {}: {}'.format(personagem_classe, personagem_classe.get_bba()))
-            bba_atual += personagem_classe.get_bba()
-        return bba_atual
+        # print('bba_atual: {}'.format(bba_atual))
+        for classe in self.classes.all():
+            # print('personagem_classe: {}'.format(classe))
+            # print('bba do {}: {}'.format(classe, classe.get_bba()))
+            bba_atual += classe.get_bba()
+        return '+{}'.format(bba_atual)
 
     class Meta:
         unique_together = ('jogador', 'nome')
 
 
 class PersonagemClasse(Model):
-    personagem = ForeignKey(Personagem, on_delete=CASCADE, related_name='personagem_classes')
+    personagem = ForeignKey(Personagem, on_delete=CASCADE, related_name='classes')
     classe = ForeignKey(Classe, on_delete=PROTECT, related_name='+')
     nivel = IntegerField()
 
@@ -70,3 +81,55 @@ class PersonagemClasse(Model):
 
     class Meta:
         unique_together = ('personagem', 'classe')
+
+
+class PersonagemModelo(Model):
+    personagem = ForeignKey(Personagem, on_delete=CASCADE, related_name='modelos')
+    modelo = ForeignKey(Modelo, on_delete=PROTECT, related_name='+')
+
+    def __str__(self):
+        return '{} | {}'.format(self.personagem, self.modelo)
+
+    @property
+    def nome(self):
+        return self.modelo.nome
+
+    class Meta:
+        unique_together = ('personagem', 'modelo')
+
+
+class PersonagemAtributo(Model):
+    personagem = ForeignKey(Personagem, on_delete=CASCADE, related_name='atributos')
+    atributo = ForeignKey(Atributo, on_delete=PROTECT, related_name='+')
+    valor = IntegerField(default=0)
+    valor_temporario = IntegerField(default=0)
+
+    def __str__(self):
+        return self.nome
+
+    @property
+    def mod(self):
+        mod = self.valor + self.valor_temporario
+        if mod%2 == 1:
+            mod -= 1
+        return '+{}'.format(int((mod-10)/2))
+
+    @property
+    def nome(self):
+        return self.atributo.nome
+
+    @property
+    def slug(self):
+        return self.atributo.slug
+
+    class Meta:
+        unique_together = ('personagem', 'atributo')
+
+def add_atributos(sender, **kwargs):
+    personagem = kwargs["instance"]
+    if kwargs["created"]:
+        atributos = Atributo.objects.all()
+        for atributo in atributos:
+            PersonagemAtributo(personagem=personagem, atributo=atributo).save()
+
+post_save.connect(add_atributos, sender=Personagem)
