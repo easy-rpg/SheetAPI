5e6ebab3b05b05ab78ef29f6f9255b90b658fdf8
RodrigoNdeC <rodrigondec@gmail.com>
2018-06-30 19:28:38 -0300

    configuração de static e media

--- SheetAPI/settings.py
+++ SheetAPI/settings.py
@@ -13,6 +13,12 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 from .config import DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD
 
+
+SETTINGS_DIR = os.path.dirname(__file__)
+PROJECT_PATH = os.path.join(SETTINGS_DIR, os.pardir)
+PROJECT_PATH = os.path.abspath(PROJECT_PATH)
+TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -26,7 +32,7 @@ SECRET_KEY = 'bfh-+v@!45=mydaor*1ji=$smfpfsy!_7dv)b^u$m80qwg3dl5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sheet-api.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -140,4 +146,34 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_PATH = os.path.join(PROJECT_PATH, 'static')
+STATIC_ROOT = os.path.join(SETTINGS_DIR, 'static')
+STATIC_URL = '/static/'  # You may find this is already defined as such.
+STATICFILES_DIRS = (
+    # os.path.join(BASE_DIR, 'static'), 
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media')  # Absolute path to the media directory
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+        },
+    },
+}
