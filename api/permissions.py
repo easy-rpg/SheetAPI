95fabf156b35f6ef5d73c7e950ad8e87b81155a8
RodrigoNdeC <rodrigondec@gmail.com>
2018-07-03 23:26:32 -0300

    IsOwnerOrRead adicionado; get_owner := owner no obj

--- api/permissions.py
+++ api/permissions.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
 class IsOwner(BasePermission):
@@ -6,5 +6,17 @@ class IsOwner(BasePermission):
     Custom permission to only allow owners of an object to edit it.
     """
     def has_object_permission(self, request, view, obj):
-        # permissions are only allowed to the owner of the personagem
-        return obj.get_owner == request.user
+        # Permissions are only allowed to the owner of the personagem
+        return obj.owner == request.user
+
+
+class IsOwnerOrRead(BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it.
+    """
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request
+        # Edit permissions are only allowed to the owner of the personagem
+        if request.method in SAFE_METHODS:
+            return True
+        return obj.owner == request.user
